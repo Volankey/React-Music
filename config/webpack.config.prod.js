2afcbe6bb16550fae43f18381d4b609b9dae32e2
Volankey <935196116@qq.com>
2018-05-11 23:14:49 +0800

    修复playall

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -104,6 +104,8 @@ module.exports = {
             // please link the files into your node_modules/ and let module-resolution kick in.
             // Make sure your source files are compiled, as they will not be processed in any way.
             new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+
+
         ],
     },
     module: {
@@ -341,6 +343,7 @@ module.exports = {
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
